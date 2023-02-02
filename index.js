725a74959d40db3cab6fa1c7ba9695a55b2961ff
Mohammad Talaat <eng.muhamedtalaat@gmail.com>
2023-02-02 10:21:43 +0200

    add delete user route

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ const linkedInRouters = require('./social_login/linkedin/linkedinRoutes')
 app.use(cors())
 app.use(express.json())
 
-app.use(session({ secret: process.env.SESSION_SECERT }));
+app.use(session({
+    secret: process.env.SESSION_SECERT,
+    resave:true,
+    saveUninitialized: true
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -25,7 +29,9 @@ app.get('/',(req,res)=>{
 app.use('/google',googleRoutes)
 app.use('/facebook',facbookRoutes)
 app.use('/linkedIn',linkedInRouters)
-
+app.get('/deleteUserData',(req,res)=>{
+    res.send('user Data is deleted')
+})
 app.listen(process.env.PORT,()=>{
     console.log(`server is running on port: ${process.env.PORT}`)
 })
