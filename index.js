641111494d63662b9fe6fae8c13c6d51da513a12
Mohammad Talaat <eng.muhamedtalaat@gmail.com>
2023-02-06 18:46:31 +0200

    try twitter sdk package

--- index.js
+++ index.js
@@ -7,17 +7,14 @@ const morgan = require('morgan')
 
 const app = express()
 const passport = require('passport')
-const googleRoutes = require('./social_login/google/googleRoutes')
-const facbookRoutes = require('./social_login/facebook/facebookRoutes')
-const linkedInRoutes = require('./social_login/linkedin/linkedinRoutes')
-const twitterRoutes = require('./social_login/twitter/twitterRoutes')
+const twitterRoutes = require('./social_login/twitterLogin/twitterRoutes')
 
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms')
 )
 
-app.use(session({
+app.use(session({ 
     secret: process.env.SESSION_SECERT,
     resave:true,
     saveUninitialized: true
@@ -29,10 +26,36 @@ app.get('/',(req,res)=>{
     res.send('hi this is the homepage')
 })
 
-app.use('/google',googleRoutes)
-app.use('/facebook',facbookRoutes)
-app.use('/linkedIn',linkedInRoutes)
-app.use('/twitter',twitterRoutes)
+/* ------twitter test----------- */
+app.get('/twitter/oauth/callback', (req, res) => { 
+    // Extract state and code from query string
+    const { state, code } = req.query;
+    // Get the saved codeVerifier from session
+    const { codeVerifier, state: sessionState } = req.session;
+  
+    if (!codeVerifier || !state || !sessionState || !code) {
+      return res.status(400).send('You denied the app or your session expired!');
+    }
+    if (state !== sessionState) {
+      return res.status(400).send('Stored tokens didnt match!');
+    }
+  
+    // Obtain access token
+    const client = new TwitterApi({ clientId: process.env.TWITTER_CLIENT_ID, clientSecret: process.env.TWITTER_CLIENT_SECRET });
+  
+    client.loginWithOAuth2({ code, codeVerifier, redirectUri: process.env.TWITTER_CALL_BACK_URL })
+      .then(async ({ client: loggedClient, accessToken, refreshToken, expiresIn }) => {
+        // {loggedClient} is an authenticated client in behalf of some user
+        // Store {accessToken} somewhere, it will be valid until {expiresIn} is hit.
+        // If you want to refresh your token later, store {refreshToken} (it is present if 'offline.access' has been given as scope)
+  
+        // Example request
+        const { data: userObject } = await loggedClient.v2.me();
+      })
+      .catch(() => res.status(403).send('Invalid verifier or access tokens!'));
+  });
+/* ------twitter test----------- */
+// app.use('/twitter',twitterRoutes)
 app.get('/deleteUserData',(req,res)=>{
     res.send('user Data is deleted')
 })
--- social_login/twitterLogin/twitterSetup.js
+++ social_login/twitterLogin/twitterSetup.js
@@ -1,17 +1,38 @@
-const passport = require('passport')
-const OAuth2Strategy = require('passport-oauth2')
+const {TwitterAPI} = require('twitter-api-v2')
 
+// const twitterClient = new TwitterAPI(process.env.BEARER_TOKEN);
+const client = new TwitterApi({ clientId: process.env.TWITTER_CLIENT_ID, clientSecret: process.env.TWITTER_CLIENT_SECRET });
+// const readOnlyClient = twitterClient.readOnly;
 
-passport.use(new OAuth2Strategy({
-    authorizationURL: 'https://www.example.com/oauth2/authorize',
-    tokenURL: 'https://www.example.com/oauth2/token',
-    clientID: EXAMPLE_CLIENT_ID,
-    clientSecret: EXAMPLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/example/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ exampleId: profile.id }, function (err, user) {
-    });
-    return cb(err, user);
+// Don't forget to specify 'offline.access' in scope list if you want to refresh your token later
+const { url, codeVerifier, state } = client.generateOAuth2AuthLink(process.env.TWITTER_CALL_BACK_URL, { scope: ['tweet.read', 'users.read', 'offline.access'] });
+
+// Redirect your user to {url}, store {state} and {codeVerifier} into a DB/Redis/memory after user redirection
+
+app.get('/callback', (req, res) => {
+  // Extract state and code from query string
+  const { state, code } = req.query;
+  // Get the saved codeVerifier from session
+  const { codeVerifier, state: sessionState } = req.session;
+
+  if (!codeVerifier || !state || !sessionState || !code) {
+    return res.status(400).send('You denied the app or your session expired!');
   }
-));
+  if (state !== sessionState) {
+    return res.status(400).send('Stored tokens didnt match!');
+  }
+
+  // Obtain access token
+  const client = new TwitterApi({ clientId: process.env.TWITTER_CLIENT_ID, clientSecret: process.env.TWITTER_CLIENT_SECRET });
+
+  client.loginWithOAuth2({ code, codeVerifier, redirectUri: process.env.TWITTER_CALL_BACK_URL })
+    .then(async ({ client: loggedClient, accessToken, refreshToken, expiresIn }) => {
+      // {loggedClient} is an authenticated client in behalf of some user
+      // Store {accessToken} somewhere, it will be valid until {expiresIn} is hit.
+      // If you want to refresh your token later, store {refreshToken} (it is present if 'offline.access' has been given as scope)
+
+      // Example request
+      const { data: userObject } = await loggedClient.v2.me();
+    })
+    .catch(() => res.status(403).send('Invalid verifier or access tokens!'));
+});
